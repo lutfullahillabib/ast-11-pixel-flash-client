59f3ad3e1577147030723a4919ff04d787539053
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-08 01:30:26 +0600

    firebase config

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB9ieO8jOvAXuqDWJOJZZ2q4v8UArr3x9Q",
+    authDomain: "ast-11-photography-with-labib.firebaseapp.com",
+    projectId: "ast-11-photography-with-labib",
+    storageBucket: "ast-11-photography-with-labib.appspot.com",
+    messagingSenderId: "36099934190",
+    appId: "1:36099934190:web:76220bd3a91736052230ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
